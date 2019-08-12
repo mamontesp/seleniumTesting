58c0e8d8893871c19bd023794e0b2ffaeaa37fa2
Andrea Montes <mamontesp@unal.edu.co>
2019-08-12 14:48:58 -0500

    First commit automation test

---
+++ src/ebayTesting/customTestEbay.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package ebayTesting;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+/**
+ * @author andreamontes
+ *
+ */
+public class customTestEbay {
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+		
+	}
+}
